96a99f2ff82768c8e0a639befa9946dcacaf703e
Salvatore <backsofangels@mail.com>
2021-02-28 13:47:44 +0100

    Add behaviour of sliding navigation drawer

--- app/src/main/java/com/salvatore/cinemates/MainActivity.kt
+++ app/src/main/java/com/salvatore/cinemates/MainActivity.kt
@@ -1,17 +1,68 @@
 package com.salvatore.cinemates
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.mainActivityDrawerLayout)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, 0, 0)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        actionBarDrawerToggle.syncState()
+
+        navigationView = findViewById(R.id.mainActivityNavigationView)
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.drawer_menu_profile -> {
+                    Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.drawer_menu_discover -> {
+                    Toast.makeText(this, "Discover", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.drawer_menu_lists -> {
+                    Toast.makeText(this, "Lists", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        drawerLayout.openDrawer(navigationView)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
