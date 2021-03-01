b79fd65cd01e4e6948d10692e77208936f54f9e0
Salvatore <backsofangels@mail.com>
2021-03-01 20:15:35 +0100

    Instatiated DiscoverFragment and movie search

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+i
+9app/src/main/java/com/salvatore/cinemates/MainActivity.kt,f\a\faa3abc6ed6295e6b0a8b9a87f2aa5473bda80f5
+r
+Bapp/src/main/java/com/salvatore/cinemates/view/DiscoverFragment.kt,9\c\9cdfdb3289a01407c5bf44006492ee80e7d445b1
--- app/src/main/java/com/salvatore/cinemates/MainActivity.kt
+++ app/src/main/java/com/salvatore/cinemates/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.salvatore.cinemates
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
+import com.salvatore.cinemates.view.DiscoverFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     private lateinit var navigationView: NavigationView
+    private var TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,17 +37,27 @@ class MainActivity : AppCompatActivity() {
         navigationView = findViewById(R.id.mainActivityNavigationView)
 
         navigationView.setNavigationItemSelectedListener { menuItem ->
+            var fragment: Fragment? = null
+
             when(menuItem.itemId) {
                 R.id.drawer_menu_profile -> {
                     Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+                    this.drawerLayout.closeDrawers()
                     true
                 }
                 R.id.drawer_menu_discover -> {
-                    Toast.makeText(this, "Discover", Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, "Instantiating DiscoverFragment")
+                    fragment = DiscoverFragment()
+                    val fragmentTransaction = supportFragmentManager.beginTransaction()
+                    Log.d(TAG, "beginning transaction to fragment")
+                    fragmentTransaction.add(R.id.mainActivityDrawerLayout, fragment)
+                    fragmentTransaction.commit()
+                    this.drawerLayout.closeDrawers()
                     true
                 }
                 R.id.drawer_menu_lists -> {
                     Toast.makeText(this, "Lists", Toast.LENGTH_SHORT).show()
+                    this.drawerLayout.closeDrawers()
                     true
                 }
                 else -> {
@@ -54,7 +68,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        drawerLayout.openDrawer(navigationView)
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START)
+        } else drawerLayout.openDrawer(navigationView)
         return true
     }
 
